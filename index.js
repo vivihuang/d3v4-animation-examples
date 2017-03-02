f43b2bddca8986498806364530a01e71bb48c4a7
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-02 11:29:30 +0800

    Remove unused structure.

--- index.js
+++ index.js
@@ -25,44 +25,4 @@ const svg = d3.select('.chart')
   .append('svg')
     .attr('width', width + margin.right + margin.left)
     .attr('height', height + margin.top + margin.bottom)
-    // .call(responsivefy)
-
-const path = d3.geoPath()
-  .projection(null)
-
-const color = d3.scaleLinear()
-  .domain([0, 50000])
-  .range(colorbrewer.Greens[7])
-
-const drawMap = (us) => {
-  svg.append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`)
-    .attr('class', 'border nation')
-    .selectAll('path')
-    .data(topojson.feature(us, us.objects.nation).features)
-    .enter().append('path')
-      .attr('d', path)
-
-  svg.append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`)
-    .attr('class', 'border counties')
-    .selectAll('path')
-    .data(topojson.feature(us, us.objects.counties).features)
-    .enter().append('path')
-      .attr('d', path)
-      .attr('class', 'county')
-      .style('fill', d => color(d.id))
-
-  svg.append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`)
-    .attr('class', 'border states')
-    .append('path')
-      .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)))
-}
-
-d3.json('us_10m.json', (error, us) => {
-  if (error) {
-    throw error
-  }
-  drawMap(us)
-})
+    .call(responsivefy)
