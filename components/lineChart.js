ff1885fb1bd96f1863d5e37d27f28194e22b318e
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-03 10:56:27 +0800

    Draw basic line chart with only one line.

--- components/lineChart.js
+++ components/lineChart.js
@@ -1,3 +1,56 @@
-export const drawLineChart = (data) => {
-  console.log('~~~~~~~~~~~~~~~~~~~', data.length)
+import { line, curveCatmullRom } from 'd3-shape'
+import { timer } from 'd3-timer'
+import { axisLeft } from 'd3-axis'
+import { select } from 'd3-selection'
+
+const drawLineChart = (symbols, xScale, yScale) => {
+  const currentLine = line()
+    .x(d => xScale(d.date))
+    .y(d => yScale(d.price))
+    .curve(curveCatmullRom.alpha(0.5))
+
+  symbols.each(function (d) {
+    const layer = select(this)
+    yScale.domain([0, d.maxPrice]);
+
+    layer.selectAll(".line")
+      .attr("d", d => currentLine(d.values))
+
+    layer.selectAll(".circle")
+      .attr('cy', d => yScale(d.values[d.values.length - 1].price))
+  })
+}
+
+export const drawMultipleLineChart = (symbols, xScale, yScale, color, width) => {
+  symbols.each(function(d) {
+    const layer = select(this)
+
+    yScale.domain([0, d.maxPrice])
+
+    layer.attr('class', 'axis y')
+      .call(axisLeft(yScale).ticks(5))
+
+    layer.append('path')
+      .attr('class', 'line')
+      .style('fill', 'none')
+      .style('stroke', '#000')
+
+    layer.append('circle')
+      .attr('class', 'circle')
+      .attr('r', 5)
+      .attr('cx', width)
+      .style('fill', d => color(d.key))
+      .style('stroke', '#000')
+
+    drawLineChart(symbols, xScale, yScale)
+
+    // timer(() => {
+    //   drawLineChart(symbols, xScale, yScale, k)
+    //   if ((k += 2) >= n - 1) {
+    //     console.log(k, n, (k += 2) >= n - 1)
+    //     drawLineChart(symbols, xScale, yScale, n - 1)
+    //     return true
+    //   }
+    // })
+  })
 }
--- index.js
+++ index.js
@@ -1,24 +1,83 @@
 import { queue } from 'd3-queue'
 import { select } from 'd3-selection'
 import { csv } from 'd3-request/index'
+import { nest } from 'd3-collection'
+import { timeParse } from 'd3-time-format'
+import { min, max, sum } from 'd3-array'
+import { scaleTime, scaleLinear, scaleOrdinal } from 'd3-scale'
+import { axisBottom, axisLeft } from 'd3-axis'
 
 import { responsivefy } from './components/responsivefy'
-import { drawLineChart } from './components/lineChart'
+import { drawMultipleLineChart } from './components/lineChart'
+
+const parseStringToFloat = string =>
+  Math.round(parseFloat(string) * 100) / 100
+
+const color = scaleOrdinal()
+  .range(['#c6dbef', '#9ecae1', '#6baed6']);
 
 const margin = { top: 20, right: 20, bottom: 20, left: 20 }
-const width = 960 - margin.right - margin.left
-const height = 600 - margin.top - margin.bottom
+const width = 480 - margin.right - margin.left
+const height = 300 - margin.top - margin.bottom
 
 const svg = select('.chart')
   .append('svg')
     .attr('width', width + margin.right + margin.left)
     .attr('height', height + margin.top + margin.bottom)
-    .call(responsivefy)
+    // .call(responsivefy)
+  .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 queue()
   .defer(csv, 'stocks.csv')
   .await((error, results) => {
     if (error) throw error
-    drawLineChart(results)
+    const parseTime = timeParse('%b %Y')
+    let data = nest()
+      .key(d => d.symbol)
+      .entries(results)
+
+    data.map(d => Object.assign(d, {
+      values: d.values.map(v => ({
+        date: parseTime(v.date),
+        price: parseStringToFloat(v.price)
+      }))
+    }))
+
+    data.map(d => Object.assign(d, {
+      maxPrice: max(d.values, v => v.price),
+      sumPrice: sum(d.values, v => v.price),
+    }))
+
+    data.sort((a, b) => b.maxPrice - a.maxPrice)
+
+    const xScale = scaleTime()
+      .range([0, width - 10])
+      .domain([
+        min(data, co => min(co.values, d => d.date)),
+        max(data, co => max(co.values, d => d.date))
+      ])
+
+    const yScale = scaleLinear().range([height / 4 - 20, 0])
+
+    svg.append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .attr('class', 'axis x')
+      .call(axisBottom(xScale).ticks(5))
+
+    svg.append('g')
+      .attr('class', 'axis y')
+      .call(axisLeft(yScale).ticks(5))
+
+    data = data.slice(0, 1)
+
+    const symbols = svg.selectAll('.symbol')
+      .data(data)
+      .enter().append('g')
+      .attr('class', 'symbol')
+      .attr('transform', (d, i) => `translate(0, ${i * height / 4 + 10})`)
+
+    drawMultipleLineChart(symbols, xScale, yScale, color, width)
+
+    symbols.exit().remove()
   })
-console.log('aaaaaaaa')
