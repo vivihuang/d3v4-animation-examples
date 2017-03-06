fc99d58145ad28f54bc81f54bf85e77be96f6244
Vivi Huang <rwhuang@thoughtworks.com>
2017-03-06 14:59:40 +0800

    Change animation type for line chart.

--- src/components/lineChart.js
+++ src/components/lineChart.js
@@ -1,32 +1,40 @@
 import { line, curveCatmullRom } from 'd3-shape'
-import { timer } from 'd3-timer'
 import { select } from 'd3-selection'
+import { transition } from 'd3-transition'
+import { easeSinInOut } from 'd3-ease'
 
-const drawLineChart = (symbols, xScale, yScale, k) => {
+const easeTransition = (delayTime = 500, durationTime = 1000) => {
+  return transition()
+    .delay(delayTime)
+    .duration(durationTime)
+    .ease(easeSinInOut)
+}
+
+const hideAllShapes = (layer, xScale, yScale, data) => {
   const currentLine = line()
     .x(d => xScale(d.date))
     .y(d => yScale(d.price))
     .curve(curveCatmullRom.alpha(0.5))
 
-  symbols.each(function (d) {
-    const layer = select(this)
+  yScale.domain([0, 0])
 
-    yScale.domain([0, d.maxPrice])
-
-    layer.selectAll('.line')
-      .attr('d', d => currentLine(d.values.slice(0, k)))
-
-    if (k === d.values.length) {
-      layer.selectAll('.circle')
-        .style('opacity', 1)
-
-      layer.selectAll('.legend')
-        .style('opacity', 1)
-    }
-  })
+  layer.selectAll('.circle')
+    .style('opacity', 0)
+    .remove()
+  layer.selectAll('.legend')
+    .style('opacity', 0)
+    .remove()
+  layer.selectAll('.line')
+    .transition(easeTransition(200, 1000))
+    .attr("d", currentLine(data.values))
 }
 
 export const drawMultipleLineChart = (svg, xScale, yScale, color, width, height, data) => {
+  const currentLine = line()
+    .x(d => xScale(d.date))
+    .y(d => yScale(d.price))
+    .curve(curveCatmullRom.alpha(0.5))
+
   const symbols = svg.selectAll('.symbol')
     .data(data)
     .enter().append('g')
@@ -34,15 +42,21 @@ export const drawMultipleLineChart = (svg, xScale, yScale, color, width, height,
     .attr('transform', (d, i) => `translate(0, ${i * height / 4 + 10})`)
 
   symbols.each(function(d) {
-    let k = 1
+    const layer = select(this)
+
     const n = d.values.length
 
-    yScale.domain([0, d.maxPrice])
-
-    const layer = select(this)
+    yScale.domain([0, 0])
 
     layer.append('path')
       .attr('class', 'line')
+      .attr('d', d => currentLine(d.values))
+
+    yScale.domain([0, d.maxPrice])
+
+    layer.selectAll('.line')
+      .transition(easeTransition())
+      .attr('d', d => currentLine(d.values))
 
     layer.append('circle')
       .attr('class', 'circle')
@@ -51,6 +65,8 @@ export const drawMultipleLineChart = (svg, xScale, yScale, color, width, height,
       .attr('cy', d => yScale(d.values[n - 1].price))
       .style('fill', d => color(d.key))
       .style('opacity', 0)
+      .transition(easeTransition(1500, 500))
+        .style('opacity', 1)
 
     layer.append('text')
       .text(d => d.key)
@@ -58,13 +74,11 @@ export const drawMultipleLineChart = (svg, xScale, yScale, color, width, height,
       .attr('transform', d => `translate(${width - 30}, ${yScale(d.values[n - 1].price)})`)
       .attr('dy', '0.3rem')
       .style('opacity', 0)
+      .transition(easeTransition(1500, 500))
+        .style('opacity', 1)
 
-    const t = timer(() => {
-      drawLineChart(symbols, xScale, yScale, k)
-      if ((k += 2) >= n) {
-        drawLineChart(symbols, xScale, yScale, n)
-        t.stop()
-      }
-    })
+    window.setTimeout(() => {
+      hideAllShapes(layer, xScale, yScale, d)
+    }, 2500)
   })
 }
